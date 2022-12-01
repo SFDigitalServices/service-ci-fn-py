f1ec748240fbd76401b9cbb5f1c54d3483788dd3
hshaosf <11635576+hshaosf@users.noreply.github.com>
2022-12-01 14:11:51 -0800

    bluebeam v1 mod

---
+++ mods/bluebeam_v1.py
@@ -0,0 +1,34 @@
+""" bluebeam v1 mod """
+import os
+import json
+import requests
+
+def process(data: dict, content: dict, method: str, _params:dict):
+    """ process content """
+    if method == "submission":
+        env = data['with']['ENV']
+        endpoint = os.environ.get(f"BLUEBEAM_{env}_URL")
+        url = f"{endpoint}/submission"
+
+        headers = {
+            "ACCESS_KEY": os.environ.get(f"BLUEBEAM_{env}_KEY")
+        }
+
+        params = {}
+        print(url)
+        print(content)
+        response = requests.post(url, headers=headers, data=json.dumps(content), \
+            params=params, timeout=5)
+        response_json = content
+        print(response.status_code)
+        print(response.content)
+        if response.status_code == 200:
+            try:
+                response_json = json.loads(response.content)
+            # pylint: disable=broad-except
+            except Exception as err:
+                print(f"jsonata.process Exception: {err}")
+                response_json = {"data": str(response.content, 'utf-8')}
+
+        return response_json
+    return content
